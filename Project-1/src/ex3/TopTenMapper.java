68e55f717cfde1ffa0d1bb35b38c9005d46576b8
davide <thekage91@gmail.com>
2015-04-18 22:20:14 +0200

    Improve mapper function

--- Project-1/src/ex3/TopTenMapper.java
+++ Project-1/src/ex3/TopTenMapper.java
@@ -2,6 +2,8 @@ package ex3;
 
 
 import java.io.IOException;
+import java.util.StringTokenizer;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -12,12 +14,33 @@ import org.apache.hadoop.mapred.Reporter;
 
 public class TopTenMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
+	private Text word1 = new Text();
+	private Text word2 = new Text();
+
 	@Override
-	public void map(LongWritable arg0, Text arg1,
-			OutputCollector<Text, IntWritable> arg2, Reporter arg3)
+	public void map(LongWritable arg0, Text value,
+			OutputCollector<Text, IntWritable> output, Reporter arg3)
 			throws IOException {
-		// TODO Auto-generated method stub
 		
+		String line = value.toString();
+		StringTokenizer tokenizer1 = new StringTokenizer(line, ","); 
+		StringTokenizer tokenizer2;
+		int pos1 = 0; 
+		int pos2 = 0;
+		
+		while(tokenizer1.hasMoreTokens()){
+			word1.set(tokenizer1.nextToken());
+			tokenizer2 = new StringTokenizer(line, ","); 
+			pos1 += 1;
+			
+			while(tokenizer2.hasMoreTokens()){
+				if(pos1 < pos2){
+					word2.set(tokenizer2.nextToken());
+					pos2 += 1;
+				}
+				output.collect(arg0, arg1);
+			}
+		}
 	}
 
 }
