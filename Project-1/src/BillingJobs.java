de21293f1cee78cd2331b5bdc31255cdf1b83425
Ugo Buonadonna <ugo.buonadonna@stud.uniroma3.it>
2015-04-23 15:10:45 +0200

    ex3 in main

--- Project-1/src/BillingJobs.java
+++ Project-1/src/BillingJobs.java
@@ -1,5 +1,3 @@
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -9,18 +7,18 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.tools.CLI;
 
 import ex2.*;
+import ex4.*;
 
 
 public class BillingJobs extends Configured implements Tool{
-	//public class sBillingJobs {
 	public int run(String[] args) throws Exception {
 
 		Configuration conf = getConf();
 		Job job1 = Job.getInstance(conf, "Trimester Statistics 1 step");
 		Job job2 = Job.getInstance(conf, "Trimester Statistics 2 step");
+		Job job3 = Job.getInstance(conf, "Associative p1->p2 statistic");
 
 		job1.setJarByClass(BillingJobs.class);
 		job1.setMapperClass(StatisticMapper.class);
@@ -29,7 +27,7 @@ public class BillingJobs extends Configured implements Tool{
 		job1.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job1, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job1, new Path("temp"));
-		job1.waitForCompletion(true);
+		
 
 		job2.setMapperClass(SecondStepMapper.class);
 		job2.setReducerClass(SecondStepReducer.class);
@@ -37,8 +35,20 @@ public class BillingJobs extends Configured implements Tool{
 		job2.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job2, new Path("temp"));
 		FileOutputFormat.setOutputPath(job2,new Path(args[1]));
-		job2.waitForCompletion(true);
+		
+		job3.setMapperClass(Associationp1p2Mapper.class);
+		job3.setReducerClass(Associationp1p2Reducer.class);
+		job3.setOutputKeyClass(Text.class);
+		job3.setOutputValueClass(Text.class);
+		FileInputFormat.addInputPath(job3, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job3,new Path(args[1]));
+		
+		//job1.waitForCompletion(true);
+		//job2.waitForCompletion(true);
+		job3.waitForCompletion(true);
 		return 0;
+		
+		
 
 	}
 
