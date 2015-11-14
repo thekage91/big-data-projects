99b0dc3627d7ebe15f404f8c7fabfaf12cf47307
Ugo Buonadonna <ugo.buonadonna@stud.uniroma3.it>
2015-11-14 17:52:10 +0100

    First test of Version 0 saving has been written

--- test/saving/save_interface.js
+++ test/saving/save_interface.js
@@ -6,29 +6,45 @@
 var request = require('supertest'),
     express = require('express'),
     util = require('../util.js'),
-    q = require('q');
+    q = require('q'),
+    should = require('should-promised');
 
 process.env.NODE_ENV = 'test';
 
 var app = require('../../app.js');
 var mongoose = app.mongoose;
-var save_interface = require('../../models/save_interface.js');
+var save_interface = require('../../models/save_interface.js'),
+    Movie0 = mongoose.model('Movie0');
 
 
-describe('Save interface', function(){
+describe('Save interface', function () {
 
-        afterEach('Clear database', function(done) {
-        mongoose.connection.db.dropDatabase(function(){
-            done()
-        })
+    describe('Version 0', function () {
+        afterEach('Clear database', function (done) {
+            mongoose.connection.db.dropDatabase(function (err, ww) {
+                done();
+            })
         });
 
 
-        it('saves one movie', function(done){
-            let movie = util.fakeMovie();
-            console.log(movie);
-            (save_interface.save(0,{movie: movie})).then((data) => {
-                console.log(data); done()},(err) => {console.error(err); done()});
+        it('saves one movie', function (done) {
+            var movie_to_save = util.fakeMovie();
+
+            var movie_saved = save_interface.save(0, {movie: movie_to_save});
+
+            movie_saved.then(() => {
+                Movie0.find({title: movie_to_save.title}, function (err, movie) {
+                    movie.should.not.be.null();
+                    movie.length.should.be.equal(1);
+                    done();
+                })
+            });
+
         })
 
+        it('saves two movies', function (done) {
+            done();
+        })
+    })
 });
+
