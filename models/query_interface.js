2aad8928a7efd433eb57c65eaef52ed70a3432ca
Ugo Buonadonna <ugo.buonadonna@stud.uniroma3.it>
2015-11-26 09:47:35 +0100

    Version 1 movie by an actor query working

--- models/query_interface.js
+++ models/query_interface.js
@@ -1,33 +1,46 @@
 /**
  * Created by ugo on 13/11/15.
  */
+'use strict';
 
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
+    ObjectId = Schema.ObjectId,
+    Ac;
 
 module.exports = {
 
     // Tutti i film di un attore
-    all_films_one_actor : function (version,actor) {
-        if(isNaN(version) || actor == undefined)
+    all_films_one_actor: function (version, actor, cb) {
+        if (isNaN(version) || actor == undefined)
             throw new Error('version and actor needed')
 
-        switch(version) {
+        switch (version) {
             case 3:
             case 4:
             case 5:
             case 0:
-                console.log(`query: Movie${version}.find({actors : {$elemMatch : ${JSON.stringify(actor)} } } )`)
-                return mongoose.model('Movie' + version)
-                    .find({actors : { $elemMatch : actor}});
+                console.log(`query: Movie${version}.find({actors : {$elemMatch : ${JSON.stringify(actor)} } } )`);
+                mongoose.model('Movie' + version)
+                    .find({actors: {$elemMatch: actor}}).then((movies) => cb(undefined, movies));
                 break;
             case 1:
             case 2:
             case 6:
-                return mongoose.model('Actor' + version).find(actor,'movies');
+                console.log(`query: Actor${version}.find(${JSON.stringify(actor)},'movies').populate('movies')`);
+                mongoose.model('Actor' + version).find(actor, 'movies').populate('movies')
+                    .exec((err, actors) => {
+                        if (err) throw err;
+                        if(actors === undefined)
+                            return cb('Error: query returned undefined');
+                        var movies = [];
+                        for (let i = 0; i < actors.length; i++)
+                            movies = movies.concat(actors[i].movies);
+                        return cb(undefined, movies);
+                    });
+
                 break;
         }
-        return result;
+        //return result;
     }
 };
--- test/queries/movie-by-actor.js
+++ test/queries/movie-by-actor.js
@@ -2,7 +2,7 @@
  * Created by ugo on 12/11/15.
  */
 
-    'use strict';
+'use strict';
 
 var express = require('express'),
     q = require('q'),
@@ -23,130 +23,30 @@ describe('Query: Retrieve all movies acted by one actor', function () {
 
         //(query_interface.all_films_one_actor('a', (util.fakeMovie()))).should.throw(Error);
 
-        try {query_interface.all_films_one_actor('A', (util.fakeMovie()));  }
-        catch (err) {err.should.be.Error();  done();}
+        try {
+            query_interface.all_films_one_actor('A', (util.fakeMovie()));
+        }
+        catch (err) {
+            err.should.be.Error();
+            done();
+        }
     });
 
     it("fails with undefined 'actor' argument", function (done) {
 
         //query_interface.all_films_one_actor(1, null).should.throw(Error);
 
-        try {query_interface.all_films_one_actor(1, undefined);  }
-        catch (err) {err.should.be.Error();  done();}
+        try {
+            query_interface.all_films_one_actor(1, undefined);
+        }
+        catch (err) {
+            err.should.be.Error();
+            done();
+        }
     });
 
 
     describe('Version 0', function () {
-
-            var saved_movie_with_1_actor;
-            var saved_movie_with_10_actor;
-            var saved_movie_with_100_actor;
-            var saved_actor_with_1_movie;
-            var saved_actor_with_10_movie;
-            var saved_actor_with_100_movie;
-            var saved_director;
-            var saved_genre;
-
-            before('Populate database', function (done) {
-
-                let all_saved = [];
-                saved_movie_with_1_actor = util.fakeMovie();
-                saved_movie_with_10_actor = util.fakeMovie();
-                saved_movie_with_100_actor = util.fakeMovie();
-                saved_actor_with_1_movie = util.fakeActor();
-                saved_actor_with_10_movie = util.fakeActor();
-                saved_actor_with_100_movie = util.fakeActor();
-                saved_director = util.fakeDirector();
-                saved_genre = util.fakeGenre();
-
-
-                all_saved.push(save_interface.save(0,
-                    {movie: saved_movie_with_1_actor, actor: saved_actor_with_1_movie,
-                        director: saved_director, genre: saved_genre }))
-
-
-                all_saved.push(save_interface.save(0,
-                    {movie: saved_movie_with_10_actor}))
-
-
-                all_saved.push(save_interface.save(0,
-                    {movie: saved_movie_with_100_actor}))
-
-                q.all(all_saved).then(() => {
-                    let all_saved = [];
-                    for(let i=0; i < 100; i++) {
-                        ( () => {
-                            //let movie = util.fakeMovie();
-                            if(i<10)
-                                all_saved.push(save_interface.save(0, {movie: util.fakeMovie(), actor: saved_actor_with_10_movie}));
-                            all_saved.push(save_interface.save(0, {movie: util.fakeMovie(), actor: saved_actor_with_100_movie}));
-                        })();
-                    }
-                    q.all(all_saved).then(() => done())
-                })
-
-
-            });
-
-            after('Clear database', function (done) {
-                mongoose.connection.db.dropDatabase(function () {
-                    done();
-                })
-            });
-
-            it('Retrieves 1 movie', function (done) {
-
-                // util.fakeActor() adds 'movies' field to Actor object, used for relationships.
-                // Version 0 model does not have relationships, only embedded documents.
-                // So we delete the field
-                delete saved_actor_with_1_movie.movies;
-
-                query_interface.all_films_one_actor(0, saved_actor_with_1_movie)
-                    .then((movies) => {
-                        movies.length.should.eql(1);
-                        movies[0].title.should.eql(saved_movie_with_1_actor.title);
-                        done();
-                    });
-            });
-
-            it('Retrieves 10 movie', function (done) {
-
-                // util.fakeActor() adds 'movies' field to Actor object, used for relationships.
-                // Version 0 model does not have relationships, only embedded documents.
-                delete saved_actor_with_10_movie.movies;
-
-                query_interface.all_films_one_actor(0, saved_actor_with_10_movie)
-                    .then((movies) => {
-                        movies.length.should.eql(10);
-                        for(let i=0; i <movies.length; i++)
-                            movies[i].actors[0].first_name.should.be.eql(saved_actor_with_10_movie.first_name);
-                        done();
-                    });
-            });
-
-        it('Retrieves 100 movie', function (done) {
-
-            // util.fakeActor() adds 'movies' field to Actor object, used for relationships.
-            // Version 0 model does not have relationships, only embedded documents.
-            delete saved_actor_with_100_movie.movies;
-
-            query_interface.all_films_one_actor(0, saved_actor_with_100_movie)
-                .then((movies) => {
-                    movies.length.should.eql(100);
-                    for(let i=0; i <movies.length; i++) {
-                        console.log(movies[i].actors.length);
-                        movies[i].actors[1] = movies[i].actors[1] || {};
-                        [ movies[i].actors[0].first_name, movies[i].actors[1].first_name]
-                            .should.containEql(saved_actor_with_100_movie.first_name);
-                    }
-                    done();
-                });
-        });
-
-    });
-
-
-    describe('Version 1', function () {
 
         var saved_movie_with_1_actor;
         var saved_movie_with_10_actor;
@@ -170,27 +70,32 @@ describe('Query: Retrieve all movies acted by one actor', function () {
             saved_genre = util.fakeGenre();
 
 
-            all_saved.push(save_interface.save(1,
-                {movie: saved_movie_with_1_actor, actor: saved_actor_with_1_movie,
-                    director: saved_director, genre: saved_genre }))
+            all_saved.push(save_interface.save(0,
+                {
+                    movie: saved_movie_with_1_actor, actor: saved_actor_with_1_movie,
+                    director: saved_director, genre: saved_genre
+                }))
 
 
-            all_saved.push(save_interface.save(1,
+            all_saved.push(save_interface.save(0,
                 {movie: saved_movie_with_10_actor}))
 
 
-            all_saved.push(save_interface.save(1,
+            all_saved.push(save_interface.save(0,
                 {movie: saved_movie_with_100_actor}))
 
             q.all(all_saved).then(() => {
                 let all_saved = [];
-                for(let i=0; i < 100; i++) {
-                    ( () => {
-                        //let movie = util.fakeMovie();
-                        if(i<10)
-                            all_saved.push(save_interface.save(1, {movie: util.fakeMovie(), actor: saved_actor_with_10_movie}));
-                        all_saved.push(save_interface.save(1, {movie: util.fakeMovie(), actor: saved_actor_with_100_movie}));
-                    })();
+                for (let i = 0; i < 100; i++) {
+                        if (i < 10)
+                            all_saved.push(save_interface.save(0, {
+                                movie: util.fakeMovie(),
+                                actor: saved_actor_with_10_movie
+                            }));
+                        all_saved.push(save_interface.save(0, {
+                            movie: util.fakeMovie(),
+                            actor: saved_actor_with_100_movie
+                        }));
                 }
                 q.all(all_saved).then(() => done())
             })
@@ -198,11 +103,11 @@ describe('Query: Retrieve all movies acted by one actor', function () {
 
         });
 
-        /* after('Clear database', function (done) {
-         mongoose.connection.db.dropDatabase(function () {
-         done();
-         })
-         });*/
+        after('Clear database', function (done) {
+            mongoose.connection.db.dropDatabase(function () {
+                done();
+            })
+        });
 
         it('Retrieves 1 movie', function (done) {
 
@@ -211,12 +116,11 @@ describe('Query: Retrieve all movies acted by one actor', function () {
             // So we delete the field
             delete saved_actor_with_1_movie.movies;
 
-            query_interface.all_films_one_actor(1, saved_actor_with_1_movie)
-                .then((movies) => {
-                    movies.length.should.eql(1);
-                    movies[0].title.should.eql(saved_movie_with_1_actor.title);
-                    done();
-                });
+            query_interface.all_films_one_actor(0, saved_actor_with_1_movie, (err, movies) => {
+                movies.length.should.eql(1);
+                movies[0].title.should.eql(saved_movie_with_1_actor.title);
+                done();
+            })
         });
 
         it('Retrieves 10 movie', function (done) {
@@ -225,13 +129,12 @@ describe('Query: Retrieve all movies acted by one actor', function () {
             // Version 0 model does not have relationships, only embedded documents.
             delete saved_actor_with_10_movie.movies;
 
-            query_interface.all_films_one_actor(0, saved_actor_with_10_movie)
-                .then((movies) => {
-                    movies.length.should.eql(10);
-                    for(let i=0; i <movies.length; i++)
-                        movies[i].actors[0].first_name.should.be.eql(saved_actor_with_10_movie.first_name);
-                    done();
-                });
+            query_interface.all_films_one_actor(0, saved_actor_with_10_movie, (err, movies) => {
+                movies.length.should.eql(10);
+                for (let i = 0; i < movies.length; i++)
+                    movies[i].actors[0].first_name.should.be.eql(saved_actor_with_10_movie.first_name);
+                done();
+            });
         });
 
         it('Retrieves 100 movie', function (done) {
@@ -240,17 +143,102 @@ describe('Query: Retrieve all movies acted by one actor', function () {
             // Version 0 model does not have relationships, only embedded documents.
             delete saved_actor_with_100_movie.movies;
 
-            query_interface.all_films_one_actor(0, saved_actor_with_100_movie)
-                .then((movies) => {
-                    movies.length.should.eql(100);
-                    for(let i=0; i <movies.length; i++) {
-                        console.log(movies[i].actors.length);
-                        movies[i].actors[1] = movies[i].actors[1] || {};
-                        [ movies[i].actors[0].first_name, movies[i].actors[1].first_name]
-                            .should.containEql(saved_actor_with_100_movie.first_name);
-                    }
-                    done();
-                });
+            query_interface.all_films_one_actor(0, saved_actor_with_100_movie, (err, movies) => {
+                movies.length.should.eql(100);
+                for (let i = 0; i < movies.length; i++) {
+                    movies[i].actors[1] = movies[i].actors[1] || {};
+                    [movies[i].actors[0].first_name, movies[i].actors[1].first_name]
+                        .should.containEql(saved_actor_with_100_movie.first_name);
+                }
+                done();
+            });
+        });
+
+    });
+
+
+    describe('Version 1', function () {
+
+        var saved_movie_with_1_actor;
+        var saved_movie_with_10_actor;
+        var saved_movie_with_100_actor;
+        var saved_actor_with_1_movie;
+        var saved_actor_with_10_movie;
+        var saved_actor_with_100_movie;
+
+
+        before('Populate database', function (done) {
+
+            let all_saved = [];
+            saved_movie_with_1_actor = util.fakeMovie();
+            saved_movie_with_10_actor = util.fakeMovie();
+            saved_movie_with_100_actor = util.fakeMovie();
+            saved_actor_with_1_movie = util.fakeActor();
+            saved_actor_with_10_movie = util.fakeActor();
+            saved_actor_with_100_movie = util.fakeActor();
+
+
+            all_saved.push(save_interface.save(1,
+                {movie: saved_movie_with_1_actor, actor: saved_actor_with_1_movie}))
+
+            q.all(all_saved).then(() => {
+                let all_saved = [];
+                for (let i = 0; i < 100; i++) {
+                        if (i < 10)
+                            all_saved.push(save_interface.save(1, {
+                                movie: util.fakeMovie(),
+                                actor: saved_actor_with_10_movie
+                            }));
+                        all_saved.push(save_interface.save(1, {
+                            movie: util.fakeMovie(),
+                            actor: saved_actor_with_100_movie
+                        }));
+                }
+                q.all(all_saved).then(() => done())
+            })
+        });
+
+        after('Clear database', function (done) {
+            mongoose.connection.db.dropDatabase(function () {
+                done();
+            })
+        });
+
+        it('Retrieves 1 movie', function (done) {
+
+            // util.fakeActor() adds 'movies' field to Actor object, used for relationships.
+            // Version 0 model does not have relationships, only embedded documents.
+            // So we delete the field
+            delete saved_actor_with_1_movie.movies;
+
+            query_interface.all_films_one_actor(1, saved_actor_with_1_movie, (err, movies) => {
+                movies.length.should.eql(1);
+                movies[0].title.should.eql(saved_movie_with_1_actor.title);
+                done();
+            });
+        });
+
+        it('Retrieves 10 movie', function (done) {
+
+            // util.fakeActor() adds 'movies' field to Actor object, used for relationships.
+            // Version 0 model does not have relationships, only embedded documents.
+            // So we delete the field
+            delete saved_actor_with_10_movie.movies;
+
+            query_interface.all_films_one_actor(1, saved_actor_with_10_movie, (err, movies) => {
+                movies.length.should.eql(10);
+                done();
+            });
+        });
+
+        it('Retrieves 100 movie', function (done) {
+
+            delete saved_actor_with_100_movie.movies;
+
+            query_interface.all_films_one_actor(1, saved_actor_with_100_movie, (err, movies) => {
+                movies.length.should.eql(100);
+                done();
+            });
         });
 
     });
